9b42c8d29907975f118c56711f55150daabd9983
higor rafael <161877058+higorbala7@users.noreply.github.com>
2024-06-12 17:19:14 -0300

    Update and rename Higor parte2.py to Higor parte3.py

--- Higor parte2.py
+++
@@ -1,9 +0,0 @@
-# Ajuste das funções trigonométricas para calcular em graus
-def sin_graus(angulo_graus):
-    return math.sin(graus_para_radianos(angulo_graus))
-
-def cos_graus(angulo_graus):
-    return math.cos(graus_para_radianos(angulo_graus))
-
-def tan_graus(angulo_graus):
-    return math.tan(graus_para_radianos(angulo_graus))
