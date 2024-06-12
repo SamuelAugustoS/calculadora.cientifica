eea52b8124a12722e49896ebbf0d20218afa9d46
esdrasvitor2811 <167093885+esdrasvitor2811@users.noreply.github.com>
2024-06-12 17:07:00 -0300

    Update Esdras parte1.py

--- Esdras parte1.py
+++ Esdras parte1.py
@@ -1,10 +1,23 @@
 import math
+import re
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
 from kivy.metrics import dp
 
 # Função para converter graus em radianos
 def graus_para_radianos(angulo_graus):
     return math.radians(angulo_graus)
+
+# Ajuste das funções trigonométricas para calcular em graus
+def sin_graus(angulo_graus):
+    return math.sin(graus_para_radianos(angulo_graus))
+
+def cos_graus(angulo_graus):
+    return math.cos(graus_para_radianos(angulo_graus))
+
+def tan_graus(angulo_graus):
+    return math.tan(graus_para_radianos(angulo_graus))
+
+def ex(numero):
+    return math.exp(numero)
